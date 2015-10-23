660730005dac201561d6143b531fa62b2d94c9be
m516 <m516@m516>
2015-10-24 01:54:10 +0800

    TOday

---
+++ language/wiki/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(unique=True, max_length=128)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Page',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=128)),
+                ('url', models.URLField()),
+                ('views', models.IntegerField(default=0)),
+                ('category', models.ForeignKey(to='wiki.Category')),
+            ],
+        ),
+    ]
